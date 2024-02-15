20df885566555315fdfa2d7276e5f129ae5f0582
andrei <ochian_a@yahoo.ro>
2024-02-15 14:34:48 +0200

    initial: Create project

---
+++ planner_meditatii/src/main/java/com/example/planner_meditatii/PlannerMeditatiiApplication.java
@@ -0,0 +1,13 @@
+package com.example.planner_meditatii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlannerMeditatiiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlannerMeditatiiApplication.class, args);
+	}
+
+}
---
+++ planner_meditatii/src/test/java/com/example/planner_meditatii/PlannerMeditatiiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.planner_meditatii;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannerMeditatiiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
