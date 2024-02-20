8b882ed334af60b1b4eed1c9e0322285a813a3a3
andrei <ochian_a@yahoo.ro>
2024-02-20 13:15:21 +0200

    feat: Add AuthenticationRequest, AuthenticationResponse and RegisterRequiest classes

---
+++ planner_meditatii/src/main/java/com/example/planner_meditatii/auth/AuthenticationRequest.java
@@ -0,0 +1,15 @@
+package com.example.planner_meditatii.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthenticationRequest {
+    private String email;
+    private String password;
+}
