8841b24dd0d8c4c1cec16e4d64f4313930852963
andrei <ochian_a@yahoo.ro>
2024-02-26 16:06:42 +0200

    feat: send email confirmation with linkcheck

---
+++ planner_meditatii/src/main/java/com/example/planner_meditatii/email/EmailSender.java
@@ -0,0 +1,5 @@
+package com.example.planner_meditatii.email;
+
+public interface EmailSender {
+    void send(String to, String email);
+}
---
+++ planner_meditatii/src/main/java/com/example/planner_meditatii/email/EmailService.java
@@ -0,0 +1,41 @@
+package com.example.planner_meditatii.email;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class EmailService implements EmailSender{
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(EmailService.class);
+    private final JavaMailSender mailSender;
+
+    /**
+     * sends an email
+     * @param to -  the user's message
+     * @param email - admin's message
+     */
+    @Override
+    @Async//indica că o metodă trebuie să fie executată într-un fir de execuție (thread) separat
+    public void send(String to, String email) {
+        try {
+            MimeMessage mimeMessage = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "utf-8");
+            helper.setText(email, true);
+            helper.setTo(to);
+            helper.setSubject("Confirm your email");
+            helper.setFrom("ochian_a@yahoo.ro");
+            mailSender.send(mimeMessage);
+        }catch (MessagingException e){
+            LOGGER.error("failed to send email", e);
+            throw new IllegalStateException("failed to send email");
+        }
+    }
+}
